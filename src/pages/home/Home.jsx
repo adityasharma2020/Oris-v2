5eb29f3fd9f2b136d6583ee3682b648c02a7774d
adityasharma2020 <adityasharma.1.tech@gmail.com>
2023-03-13 19:04:10 +0530

    radial chart added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import './home.scss';
 import Widget from '../../components/widgets/Widget';
-import Chart from '../../components/chart/Chart';
 import Featured from '../../components/featured/Featured';
 import FileInput from '../../components/fileInput/FileInput';
 import { DataContext } from '../../context/DataContext';
@@ -20,14 +19,34 @@ const Home = () => {
           <>
             {/*-------- widgets -----------*/}
             <div className='widgets'>
-              <Widget className='first' type='user' color='#fe5858' currentFile={currentFile} />
-              <Widget className='second' type='order' color='#f3ab4c' currentFile={currentFile}/>
-              <Widget className='third' type='earning' color='#b3b3b3' currentFile={currentFile}/>
-              <Widget className='fourth' type='balance' color='#44e59d' currentFile={currentFile}/>
+              <Widget
+                className='first'
+                type='user'
+                color='#fe5858'
+                currentFile={currentFile}
+              />
+              <Widget
+                className='second'
+                type='order'
+                color='#f3ab4c'
+                currentFile={currentFile}
+              />
+              <Widget
+                className='third'
+                type='earning'
+                color='#b3b3b3'
+                currentFile={currentFile}
+              />
+              <Widget
+                className='fourth'
+                type='balance'
+                color='#44e59d'
+                currentFile={currentFile}
+              />
             </div>
             <div className='main'>
               <Featured />
-              <Chart />
+            
             </div>
           </>
         ) : (
